75302f3f9a7501ab5a54229a22e243a27ba4306d
inesgarcia-nervion <ines.garcia@iesnervion.es>
2026-02-04 19:14:32 +0100

    Inicio Figma

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,12 @@
 import { Routes } from '@angular/router';
 import { BookSearchComponent } from './components/book-search/book-search.component';
 import { LoginComponent } from './components/login/login.component';
+import { InicioComponent } from './components/inicio/inicio.component';
 import { AuthGuard } from './services/auth.guard';
 import { RegisterComponent } from './components/register/register.component';
 
 export const routes: Routes = [
-	{ path: '', redirectTo: 'login', pathMatch: 'full' },
+	{ path: '', component: InicioComponent },
 	{ path: 'login', component: LoginComponent },
 	{ path: 'register', component: RegisterComponent },
 	{ path: 'search', component: BookSearchComponent, canActivate: [AuthGuard] },
